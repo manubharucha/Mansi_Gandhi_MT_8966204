20a9512b99455f9413592110c3b5a22f05457572
manu bharucha <manubharucha1631@gmail.com>
2024-02-27 10:26:49 -0800

    Initial Commit

---
+++ Mansi_Gandhi_MT_8966204/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mansi_Gandhi_MT_8966204
+//
+//  Created by user240208 on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
